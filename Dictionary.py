649519a6a483fa1c6b35c597e251b3386abee8d7
ronyahmed229 <56862644+ronyahmed229@users.noreply.github.com>
2019-10-21 23:51:23 -0700

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+studentId = {
+
+
+   "101" : "Rony",
+   "102" : "Rony1",
+   "103" : "Rony2",
+
+}
+
+print(studentId.get("104", "This is invalid"))
---
+++ escape sequence and coment.py
@@ -0,0 +1,6 @@
+print("hello wor\brld")
+print("hello \n world")
+print("hello\t world")
+print("hello \\world")
+# print("\'hello\'") control + /=# comment hoa jai
+
